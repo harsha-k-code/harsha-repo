441c0b16b0181fabedc01ff559486e9eb38c51ad
harsha <harsha.koulali@gmail.com>
2021-02-07 09:27:33 +0530

    Harsha's First commit

---
+++ src/test/java/smokeTestScripts/LoginTest.java
@@ -0,0 +1,13 @@
+package smokeTestScripts;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	public void Login()
+	{
+	System.out.println("Welcome to login page");	
+	}
+	
+}
